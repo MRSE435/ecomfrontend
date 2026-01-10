36558702f4d1a06c4242c0e918f28218df898481
MRSE435 <m54655967@gmail.com>
2026-01-10 22:43:10 +0530

    added responisvesness to cart page

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -16,14 +16,14 @@ const Cart = ({ cart, increment, decrement, deleteitem }) => {
                             <div className="cartitem w-full shadow-md p-3 flex" key={cartitem.productid._id}>
                                 <div className="image  flex gap-6 text-xl ">
                                     <img src={`${import.meta.env.VITE_API_URL}${cartitem.productid.imagePath}`}
-                                        alt="" className='w-[35%] object-contain rounded-md' />
-                                    <div className='flex flex-col gap-2'>
-                                        <h1>{cartitem.productid.name}</h1>
+                                        alt="" className='productimage w-[35%] object-contain rounded-md' />
+                                    <div className=' cartproductinfo flex flex-col gap-2'>
+                                        <h1 className='productname'>{cartitem.productid.name}</h1>
                                         <h1 className='text-green-400'>${cartitem.productid.price}</h1>
                                     </div>
 
                                 </div>
-                                <div className='flex justify-center items-center w-[30%] gap-4'>
+                                <div className='flex justify-center items-center  gap-4 incremdecrembtn'>
                                     <div className='flex   rounded-tl-md rounded-bl-md  rounded-tr-md rounded-br-md'>
                                         <div className='border-l-2 border-t-2 border-b-2  border-gray-100 pl-2 pr-2  rounded-tl-md rounded-bl-md text-blue-400 text-xl '>
                                             <button className='p-1' onClick={() => { increment(cartitem.productid._id) }}>+</button>
@@ -37,7 +37,7 @@ const Cart = ({ cart, increment, decrement, deleteitem }) => {
                                     </div>
 
                                     <button onClick={() => { deleteitem(cartitem.productid._id) }}>
-                                        <button className="p-2 bg-red-500 rounded-lg hover:bg-red-600 transition">
+                                        <button className="p-2 deletebutton bg-red-500 rounded-lg hover:bg-red-600 transition">
                                             <svg
                                                 xmlns="http://www.w3.org/2000/svg"
                                                 viewBox="0 0 24 24"
@@ -64,12 +64,12 @@ const Cart = ({ cart, increment, decrement, deleteitem }) => {
 
                     })}
                 </div>
-                <div className="second w-[30%]  flex justify-center ">
+                <div className="second w-[30%]  flex justify-center  p-4">
 
-                    <div className="total w-[80%] shadow-md h-[40%] text-2xl flex flex-col gap-9 p-4 border-2 border-gray-100">
+                    <div className="totalseconddiv w-[80%] shadow-md h-[50%] text-2xl flex flex-col gap-9 p-4 border-2 border-gray-100">
                      <h1>Total Amount:</h1>
                       <h2 className='self-end text-green-400'>$92000</h2>
-                      <button className='bg-blue-500 p-1 rounded-lg text-white'>Proceed To Checkout</button>
+                      <button className=' bg-blue-500   p-1 rounded-lg  secondcheckoutbtn   whitespace-nowrap text-white'>Proceed To Checkout</button>
                     </div>
 
                 </div>
@@ -85,7 +85,7 @@ const Cart = ({ cart, increment, decrement, deleteitem }) => {
                 </div>
                 <div className="total text-2xl flex flex-col gap-7 self-end ">
                     <h1>Total Amount:$9200</h1>
-                    <button className='bg-blue-500 p-2 rounded-xl'>Proceed to Checkout</button>
+                    <button className='bg-blue-500  checkoutbottombtn p-2 rounded-xl'>Proceed to Checkout</button>
                 </div>
             </div>
 
