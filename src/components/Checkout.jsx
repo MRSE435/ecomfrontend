e464f3da4a96086bf205b760bb7be704966c1bfe
MRSE435 <m54655967@gmail.com>
2026-05-02 21:14:05 +0530

    updated the cehckout page

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -9,32 +9,33 @@ const Checkout = ({cart})=> {
 
     <div className='w-screen h-screen m-auto p-6 m' >
       {/* main div  below*/}
-      <div className=" bg-amber-900 w-[full] h-[92%] flex mx-auto">
+      <div className=" w-[full] h-[92%] flex mx-auto">
         {/* left div */}
-        <div className="adressblockleft w-[65%] border-4 h-full">
+        <div className="adressblockleft w-[65%] h-full">
           {/* adress child divs */}
           <div className='adress field p-6 h-[35%] ' >
-            <textarea name="adress block" id="" className='w-full h-full border-2'>
-              hello
-            </textarea>
+              {/* first adress block */}
+              <div name="adress block" id="" className='w-full h-full border-2'>
+
+              </div>
           </div>
 
           <div className='OrderDetails field p-6 h-[35%]' >
-            <textarea name="adress block" id="" className='w-full h-full border-2'>
-              hello
-            </textarea>
+            <div name="adress block" id="" className='w-full h-full border-2'>
+
+            </div>
           </div>
 
           <div className='OrderDetails field p-6 h-[30%]' >
-            <textarea name="adress block" id="" className='w-full h-full border-2'>
-              hello
-            </textarea>
+            <div  name="adress block" id="" className='w-full h-full border-2'>
+
+            </div>
           </div>
 
         </div>     {/*left div close */}
 
-        <div className='rightdiv w-[35%] h-full bg-blue-600 p-6'>
-          <div className='border-2 flex  '>
+        <div className='rightdiv w-[35%] h-full p-6'>
+          <div className=' flex  '>
                  <div className='checkoutbox w-[70%] h-[67%] border-2 ml-6'>
             <section className='border-2 p-2 flex flex-col gap-4 border-b-2'>
               <section className='w-full '>
